12e4a12105eeecaa3fe755b67933eb99c86baa81
buithanhhai <buithanhhai@luvina.net>
2025-04-14 16:19:30 +0700

    create webshop-be

---
+++ src/main/java/vn/dungnt/webshop_be/WebshopBeApplication.java
@@ -0,0 +1,13 @@
+package vn.dungnt.webshop_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/dungnt/webshop_be/WebshopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.dungnt.webshop_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
